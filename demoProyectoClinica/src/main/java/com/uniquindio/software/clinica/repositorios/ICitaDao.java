0097d159f70fe8e19f8dcbfd23ddd87c4d1742f5
Fek <nicolas.ramirezr@uqvirtual.edu.co>
2023-10-28 13:19:04 -0500

    V 1.1
    Se agrega la funcionalidad para verificar la disponibilidad de horas en base a una fecha especifica al momento de agendar una cita.

--- demoProyectoClinica/src/main/java/com/uniquindio/software/clinica/repositorios/ICitaDao.java
+++ demoProyectoClinica/src/main/java/com/uniquindio/software/clinica/repositorios/ICitaDao.java
@@ -3,5 +3,9 @@ package com.uniquindio.software.clinica.repositorios;
 import com.uniquindio.software.clinica.modelo.Cita;
 import org.springframework.data.repository.CrudRepository;
 
+import java.sql.Date;
+import java.util.List;
+
 public interface ICitaDao extends CrudRepository<Cita, Integer> {
+    List<Cita> findByFechaCita(Date fecha_cita);
 }
--- demoProyectoClinica/src/main/java/com/uniquindio/software/clinica/servicios/CitaService.java
+++ demoProyectoClinica/src/main/java/com/uniquindio/software/clinica/servicios/CitaService.java
@@ -3,6 +3,7 @@ package com.uniquindio.software.clinica.servicios;
 import com.uniquindio.software.clinica.modelo.Cita;
 import com.uniquindio.software.clinica.modelo.Usuario;
 
+import java.sql.Date;
 import java.util.List;
 
 public interface CitaService {
@@ -10,4 +11,5 @@ public interface CitaService {
     Cita guardar(Cita cita);
     void eliminar(Cita cita);
     Cita buscarPorId(int id)throws Exception;
+    List<Cita> findByFechaCita(Date fecha_cita);
 }
